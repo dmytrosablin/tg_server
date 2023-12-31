d1e68ab9a66b2e24fd451b9cf3e09d0f06baf2a1
Dmytro <dmytrosablin@gmail.com>
2024-01-01 00:42:39 +0200

    leaders

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-import json
+import urllib.request
+import requests
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 
-bot = Bot('6480737039:AAFf475pQ3Pe2ESj1jZ1fqZxzrRIybUjKFg')
+bot = Bot('6602794619:AAEspg2Ru9CL3XvUInibD_uVBrTLVepmpZc')
 
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'play'])
 async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Open game', web_app=WebAppInfo(url='https://mario-16zr.onrender.com/')))
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Open game', web_app=WebAppInfo(url="https://liltendo.onrender.com/")))
     await message.answer('Hello, my friend!', reply_markup=markup)
 
-@dp.message_handler(content_types=['web_app_data'])
-async def web_app(message: types.Message):
-    res = json.loads(message.web_app_data.data)
-    await message.answer("Your score is "+ str(res["score"]))
-    print("Send")
+@dp.message_handler(commands=['record'])
+async def record(message: types.Message):
+    # contents = urllib.request.urlopen('https://liltendo.onrender.com/api/'+str(message.from_id)).read()
+    contents = requests.get('https://liltendo.onrender.com/api/'+str(message.from_id))
+    await message.answer('Your record is '+ contents.text)
+
+
+@dp.message_handler(commands=['leaders'])
+async def lider(message: types.Message):
+    await message.answer("These are our leaders: ")
+    contents = requests.get('https://liltendo.onrender.com/api/get_liders/123123').json()
+    for i in range(len(contents)):
+        await message.answer(contents[i]["record"])
+@dp.message_handler()
+async def echo(message: types.Message):
+    print(message.text)
+    await message.answer(message.text)
+
 
 executor.start_polling(dp)
